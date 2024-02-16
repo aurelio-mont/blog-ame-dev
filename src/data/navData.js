cbc24acbcfffc45d270683081d605dafb0ab7977
aurelio-mont <aurelio.mon@me.comt>
2024-02-16 14:23:07 -0600

    Nav Bar

---
+++ src/data/navData.js
@@ -0,0 +1,18 @@
+const navData = [
+    {
+        name: "About",
+        path: "/about/",
+        icon: {
+            name: "about",
+        } 
+    },
+    {
+        name: "Blog",
+        path: "/blog/",
+        icon: {
+            name: "blog",
+        },
+    },
+]
+
+export default navData;
